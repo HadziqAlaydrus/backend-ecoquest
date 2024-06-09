e30bbae3a684f7104c62069aa1c0897208c215a5
hadziqalaydrus <hadziq.alaydrus361@gmail.com>
2024-06-09 17:37:51 +0700

    update : format date

--- src/controllers/form.js
+++ src/controllers/form.js
@@ -1,11 +1,21 @@
 const { query } = require("express");
 const db = require("../config/db");
 
+//format date
+const formatDate = (dateString) => {
+    const options = { year: "numeric", month: "2-digit", day: "2-digit" };
+    return new Date(dateString).toLocaleDateString("en-CA", options);
+};
+
 //get user
 const getPickUp = async (req, res) => {
     try {
         const result = await db.query('SELECT * FROM pickup');
-        res.status(200).json(result.rows);
+        const formattedData = result.rows.map(item => ({
+            ...item,
+            tanggal_pengambilan: formatDate(item.tanggal_pengambilan)
+        }));
+        res.status(200).json(formattedData);
     } catch (error) {
         console.error( error);
         res.status(500).send('Internal server error');
@@ -44,6 +54,8 @@ const getFormById = async (req,res) => {
         const {id} = req.params;
         const result = await db.query('select * from pickup where id = $1', [id]);
         if (result.rows.length > 0 ){
+            const item = result.rows[0];
+            item.tanggal_pengambilan = formatDate(item.tanggal_pengambilan);
             res.status(200).json(result.rows[0]);
         } else{
             res.status(404).send('Form Not Found');
