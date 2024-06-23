4d7f19923976daf7f6cf0811fe2730ba997ff5ea
hadziqalaydrus <hadziq.alaydrus361@gmail.com>
2024-06-23 15:56:54 +0700

    db1

--- src/config/db.js
+++ src/config/db.js
@@ -1,6 +1,9 @@
 const {Pool} = require('pg');
 require('dotenv').config();
 
+console.log("Database URL: ", process.env.POSTGRES_URL);
+console.log("Database Password Type: ", typeof process.env.DB_PASSWORD);
+
 const pool = new Pool({
     connectionString: process.env.POSTGRES_URL,
 });
